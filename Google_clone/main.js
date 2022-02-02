629e2b2f9efcf614b8bbf19941eb9cafd414328f
miteshpr97 <miteshpradhan97@gmail.com>
2022-02-02 21:32:55 +0530

    update

--- Google_clone/main.js
+++ Google_clone/main.js
@@ -1,4 +1,7 @@
 const searchInput = document.querySelector("#search-input");
+const googlesearch = document.querySelector('.google-search-btn')
+
+googlesearch.addEventListener('click' , search);
 
 searchInput.addEventListener("keydown", function(event) {
     if(event.code === "Enter") {
@@ -10,4 +13,4 @@ function search() {
     const input = searchInput.value;
 
     window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8"
-}
+} 
